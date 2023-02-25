6a8ef4112b3e03e7277cce13346c2bcd5cd203b1
Illia <illiaholodniak@gmail.com>
2023-02-25 20:45:40 +0200

    initial API

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use actix_web::{web, App, HttpResponse, HttpServer, Responder};
 
+mod api;
+
 // "/ping", to check the functionality
 async fn ping() -> impl Responder {
     HttpResponse::Ok().body("Pong")
@@ -7,11 +9,20 @@ async fn ping() -> impl Responder {
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    dotenv::dotenv().ok();
+
+    let host = std::env::var("HOST").unwrap_or_else(|_| "localhost".to_owned());
+    let port = std::env::var("PORT").unwrap_or_else(|_| "8080".to_owned());
+    let addr = format!("{}:{}", host, port);
+
+    println!("API is available at <http://{addr}/api/get_meme>");
+
     HttpServer::new(|| {
         App::new()
             .route("/ping", web::get().to(ping))
+            .configure(api::init_routes)
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(addr)?
     .run()
     .await
 }
